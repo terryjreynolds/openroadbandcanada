6912384a173aa1c5bcbd87fd3f51428aad084edc
Terry Reynolds <terrancejreynolds@gmail.com>
2025-02-28 22:11:12 -0500

    got images loading

---
+++ app/footer.tsx
@@ -0,0 +1,29 @@
+// import Image from "next/image";
+import styles from "./page.module.css";
+// import Link from 'next/link';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebookF, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'; 
+
+
+
+export default function Footer() {
+  return (
+    //overlay
+    <div >
+      <div className={styles.pagetext}>
+<strong>
+<span>
+JUST DRIVE IT!
+</span>
+</strong>
+<h2>
+The Great Home of the Soul is the Open Road...!
+</h2>
+<h2>
+D.H. Lawrence
+</h2>
+</div>
+    </div>
+    
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 // import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./header";
+import Footer from "./footer";
+// import Image from 'next/image';
 // import Link from 'next/link';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faFacebookF, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'; 
@@ -10,22 +12,30 @@ import Header from "./header";
 export default function Home() {
   return (
     //overlay
-    <div className={styles.page}>
-<div >
+    <div className={styles.fixedbackground}>
+<div>
       <Header />
       </div>
-<div className={styles.pagetext}>
-<strong>
-<span>
-JUST DRIVE IT!
-</span>
-</strong>
-<h2>
-The Great Home of the Soul is the Open Road...!
-</h2>
-<h2>
-D.H. Lawrence
-</h2>
+
+<div>
+{/* <Image
+      src={'/ecotaybandpic.jpg'}
+      alt="Band photo at Ecotay"
+      width={1000} 
+      height={800} 
+      blurDataURL="data:..." 
+      placeholder="blur" // Optional blur-up while loading
+      
+    /> */}
+</div>
+      
+
+      
+
+
+
+<div>
+  <Footer/>
 </div>
 
     </div>
