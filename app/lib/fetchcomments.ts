6554b1bc9ed8e2cffa13ef7fb402595fd8b4a30f
Terry Reynolds <terrancejreynolds@gmail.com>
2025-03-26 01:04:51 +0000

    adjustments to comments routing

--- app/lib/fetchcomments.ts
+++ app/lib/fetchcomments.ts
@@ -1,17 +1,13 @@
 
 export async function fetchComments() {
   const url = process.env.GOOGLE_SHEETS_ENDPOINT_URL as string; //production endpoint
-  const devUrl = process.env.NEXT_PUBLIC_API_URL as string; //development endpoint
-  console.log("Fetching comments from:", devUrl);
+  // const devUrl = process.env.NEXT_PUBLIC_API_URL as string; //development endpoint
+  // console.log("Fetching comments from:", devUrl);
 
   let res = await fetch(url); 
-
+console.log('res', res);
   if (!res.ok) {
-    console.warn(`Primary fetch failed, falling back to devUrl: ${res.statusText}`);
-    res = await fetch(devUrl); 
-    if (!res.ok) {
-      throw new Error(`Failed to fetch comments from both endpoints: ${res.statusText}`);
-    }
+    throw new Error(`Failed to fetch comments`);
   }
 
 //   if (!res.ok) {
